76887798630658b4d5d31542547deba6e48fe178
Stefan Rosca <roscas@gb-lt-mj33036.local>
2017-01-02 18:14:34 +0000

    Lesson 3 - State and Touch Events

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -2,26 +2,96 @@ import React, { Component } from 'react';
 import {
     Text,
     StyleSheet,
-    View
+    View,
+    TextInput,
+    TouchableHighlight,
+    ActivityIndicatorIOS
 } from 'react-native';
 
 export default class Main extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            isLoading: false,
+            error: false
+        }
+    }
+
+    handleChange(event){
+        this.setState({
+            username: event.nativeEvent.text
+        })
+    }
+    handleSubmit(){
+        this.setState({
+            isLoading: true
+        });
+        console.log('SUBMIT', this.state.username);
+        // get data from github
+        // reroute in to next state
+    }
     render() {
         return (
             <View style={styles.mainContainer}>
-                <Text>Testing the router</Text>
+                <Text style={styles.title}>Search for a Github user</Text>
+                <TextInput
+                    style={styles.searchInput}
+                    value={this.state.username}
+                    onChange={this.handleChange.bind(this)}
+                />
+                <TouchableHighlight
+                    style={styles.button}
+                    onPress={this.handleSubmit.bind(this)}
+                    underlayColor='white'
+                >
+                    <Text style={styles.buttonText}>SEARCH</Text>
+                </TouchableHighlight>
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-        mainContainer: {
-            flex: 1,
-            padding: 30,
-            marginTop: 65,
-            flexDirection: 'column',
-            justifyContent: 'center',
-            backgroundColor: '#48BBEC'
-        }
-    });
+    mainContainer: {
+        flex: 1,
+        padding: 30,
+        marginTop: 65,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        backgroundColor: '#48BBEC'
+    },
+    title: {
+        marginBottom: 20,
+        fontSize: 25,
+        textAlign: 'center',
+        color: '#fff'
+    },
+    searchInput: {
+        height: 50,
+        padding: 4,
+        marginRight: 5,
+        fontSize: 23,
+        borderWidth: 1,
+        borderColor: 'white',
+        borderRadius: 8,
+        color: 'white'
+    },
+    buttonText: {
+        fontSize: 18,
+        color: '#111',
+        alignSelf: 'center'
+    },
+    button: {
+        height: 45,
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        borderColor: 'white',
+        borderWidth: 1,
+        borderRadius: 8,
+        marginBottom: 10,
+        marginTop: 10,
+        alignSelf: 'stretch',
+        justifyContent: 'center'
+    },
+});
